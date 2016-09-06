428013d5d5b2aa2f0c1c6d9fb61e4a64d334b47f
Dom B <dbaul1920@gmail.com>
2016-09-06 17:41:38 -0400

    Create appDayOne.java
    This is a app I wrote as part of a lab at Grand Circus Java bootcamp :)

---
+++ appDayOne.java
@@ -0,0 +1,52 @@
+/* 
+This is a app I wrote as part of a lab at Grand Circus Java bootcamp :)
+*/
+
+package HeyYou;
+
+import java.util.Scanner;
+
+public class appDayOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String choice;
+do{		
+		Scanner input = new Scanner(System.in);
+		
+		//Greet user
+		System.out.println("Welcome to Grand Circus' Room Detail Genirator");
+		
+		//Enter length		
+		double length;
+		System.out.println("Enter Length: ");
+		length = input.nextDouble();
+		
+		//Enter width
+		int width;
+		System.out.println("Enter Width: ");
+		width = input.nextInt();
+		
+		//Display area
+		int area = (int) (length * width);
+		System.out.println("Area: " + area);
+		
+		//Display perimeter
+		double perimeter = (length + width) * 2;
+		System.out.println("Perimeter: "+ perimeter);
+		 
+		//Prompt user to continue
+		System.out.println("Continue? (y/n): ");
+		choice = input.nextLine();	
+		choice = input.nextLine();
+}		
+		//If yes repeat previous, if not do say thanks
+	
+		while (choice.equalsIgnoreCase("y")); {
+			}
+		 if (choice.equalsIgnoreCase("y")); {
+				System.out.println("See you later!");
+		}
+	}
+		}
+		
